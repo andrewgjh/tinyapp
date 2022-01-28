66f13d87199dc4879d199e457bcb8e6b3feb4ee8
Andrew Ho <andrew.gjh@gmail.com>
2022-01-28 10:05:40 -0500

    refactored code for resubmission

--- express_server.js
+++ express_server.js
@@ -6,10 +6,9 @@ const {
   passwordCheck
 } = require('./helpers');
 const bcrypt = require('bcryptjs');
-var cookieSession = require('cookie-session');
+const cookieSession = require('cookie-session');
 const express = require('express');
-var methodOverride = require('method-override');
-const e = require('express');
+const methodOverride = require('method-override');
 const app = express();
 const PORT = 8080;
 
@@ -21,9 +20,9 @@ app.use(cookieSession({
 app.use(express.urlencoded({
   extended: true
 }));
-
 app.set('view engine', 'ejs');
 
+//databases
 const users = {
   "userRandomID": {
     id: "userRandomID",
@@ -31,13 +30,14 @@ const users = {
     password: "purple-monkey-dinosaur"
   },
 }
-
 const urlDatabase = {
   "b6UTxQ": {
     longURL: "https://www.tsn.ca",
     userID: "aJ48lW"
   },
 };
+
+//root redirects to url list page
 app.get('/', (req, res) => {
   if (req.session['user_id']) {
     res.redirect('/urls');
@@ -45,6 +45,8 @@ app.get('/', (req, res) => {
     res.redirect('/login');
   }
 });
+
+//login and register routes
 app.get('/login', (req, res) => {
   if (req.session['user_id']) {
     res.redirect('/urls')
@@ -54,6 +56,7 @@ app.get('/login', (req, res) => {
   }
   res.render("user_login", templateVars);
 });
+
 app.post('/register', (req, res) => {
   const email = req.body.email;
   const passwordEntry = req.body.password;
@@ -73,59 +76,6 @@ app.post('/register', (req, res) => {
   res.redirect('/urls');
 });
 
-app.get('/register', (req, res) => {
-  if (req.session['user_id']) {
-    res.redirect('/urls');
-  }
-  const templateVars = {
-    cookie: req.session,
-  }
-  res.render('user_registration', templateVars);
-});
-
-app.get('/urls', (req, res) => {
-  const urlList = urlsForUser(req.session['user_id'], urlDatabase);
-  const templateVars = {
-    cookie: req.session,
-    urls: urlList,
-  }
-  res.render('urls_index', templateVars);
-});
-app.get('/urls/new', (req, res) => {
-  if (!req.session['user_id']) {
-    res.redirect('/login');
-  }
-  const templateVars = {
-    cookie: req.session,
-  }
-  res.render('urls_new', templateVars);
-});
-
-app.post("/urls", (req, res) => {
-  if (req.session['user_id']) {
-    let randomID = generateRandomString(6);
-    urlDatabase[randomID] = {
-      longURL: req.body.longURL,
-      userID: req.session['user_id'],
-      created: new Date().toDateString(),
-    }
-    res.redirect(`/urls/${randomID}`);
-  }else{
-    res.status(400).send("Please log in to create new URLs.")
-  }
-});
-app.delete("/urls/:shortURL/", (req, res) => {
-  if (req.session["user_id"] !== urlDatabase[req.params.shortURL].userID) {
-    res.status(400).send("Unable to perform this action.");
-  } else {
-    delete urlDatabase[req.params.shortURL];
-    res.redirect("/urls");
-  }
-});
-app.post("/urls/:id", (req, res) => {
-  urlDatabase[req.params.id].longURL = req.body.newURL
-  res.redirect("/urls");
-});
 app.post("/login", (req, res) => {
   const {
     email,
@@ -141,11 +91,53 @@ app.post("/login", (req, res) => {
     res.status(403).send('User cannot be found. Please register an account.');
   }
 });
-app.post("/logout", (req, res) => {
-  req.session['user_id'] = null;
-  res.redirect('/urls');
+app.get('/register', (req, res) => {
+  if (req.session['user_id']) {
+    res.redirect('/urls');
+  }
+  const templateVars = {
+    cookie: req.session,
+  }
+  res.render('user_registration', templateVars);
 });
 
+//main page with url list
+app.get('/urls', (req, res) => {
+  const urlList = urlsForUser(req.session['user_id'], urlDatabase);
+  const templateVars = {
+    cookie: req.session,
+    urls: urlList,
+  }
+  res.render('urls_index', templateVars);
+});
+
+//pages to create/view/edit a shortUrl 
+app.get('/urls/new', (req, res) => {
+  if (!req.session['user_id']) {
+    res.redirect('/login');
+  }
+  const templateVars = {
+    cookie: req.session,
+  }
+  res.render('urls_new', templateVars);
+});
+app.post("/urls/:id", (req, res) => {
+  urlDatabase[req.params.id].longURL = req.body.newURL
+  res.redirect("/urls");
+});
+app.post("/urls", (req, res) => {
+  if (req.session['user_id']) {
+    let randomID = generateRandomString(6);
+    urlDatabase[randomID] = {
+      longURL: req.body.longURL,
+      userID: req.session['user_id'],
+      created: new Date().toDateString(),
+    }
+    res.redirect(`/urls/${randomID}`);
+  } else {
+    res.status(400).send("Please log in to create new URLs.")
+  }
+});
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   if (!urlDatabase[shortURL]) {
@@ -161,15 +153,35 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+//deleting any selected urls
+app.delete("/urls/:shortURL/", (req, res) => {
+  const sessionId = req.session["user_id"];
+  const urlUser = urlDatabase[req.params.shortURL].userID;
+  if (sessionId !== urlUser) {
+    res.status(400).send("Unable to perform this action.");
+  } else {
+    delete urlDatabase[req.params.shortURL];
+    res.redirect("/urls");
+  }
+});
+
+//links to actually site -longURL
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   if (shortURLExist(shortURL, urlDatabase)) {
     const longURL = urlDatabase[shortURL].longURL;
     res.redirect(longURL);
-  }else{
+  } else {
     res.status(400).send("This link doe not exist.");
   }
 });
+
+app.post("/logout", (req, res) => {
+  req.session['user_id'] = null;
+  res.redirect('/urls');
+});
+
+//catch all to return user to urls list if link does not exist
 app.get('/*', (req, res) => {
   res.redirect('/urls');
 });
